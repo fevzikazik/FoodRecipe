deb93e2ac86b96948dbe10f3743a76782c20b53f
Feyzi KAZIK <37486885+fevzikazik@users.noreply.github.com>
2022-01-08 16:58:50 +0300

    Update Recipe.js

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -5,14 +5,122 @@ import {
     Image,
     TouchableOpacity,
     Animated,
-    Platform
+    Platform,
+    StyleSheet
 } from 'react-native';
 
 import { BlurView } from 'expo-blur'
 
-import { SIZES, FONTS, COLORS, icons} from '../constants'
+import { SIZES, FONTS, COLORS, icons } from '../constants'
 
-const HEADER_HEIGHT = SIZES.height/2.5;
+const HEADER_HEIGHT = SIZES.height / 2.5;
+
+const RecipeCreatorCardDetail = ({selectedRecipe}) => {
+    return (
+        <View
+            style={{
+                flex: 1,
+                flexDirection: 'row',
+                alignItems: 'center'
+            }}
+        >
+            {/* Profile Photo */}
+            <View
+                style={{
+                    width: 40,
+                    height: 40,
+                    marginLeft: 20
+                }}
+            >
+                <Image 
+                    source={selectedRecipe?.author?.profilePic}
+                    style={{
+                        width: 45,
+                        height: 45,
+                        borderRadius: 20
+                    }}
+                />
+            </View>
+
+            {/* Labels */}
+            <View
+                style={{
+                    flex: 1,
+                    marginHorizontal: 20,
+                }}
+            >
+                <Text style={{color: COLORS.lightGreen1, ...FONTS.body4}}>Recipe by:</Text>
+                <Text style={{color: COLORS.white2, ...FONTS.h3}}>{selectedRecipe?.author?.name}</Text>
+            </View>
+
+            {/* Button */}
+            <TouchableOpacity
+                style={{
+                    width: 30,
+                    height: 30,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginRight: 20,
+                    borderRadius: 5,
+                    borderWidth: 1,
+                    borderColor: COLORS.lightGreen1
+                }}
+                onPress={() => console.log('View Profile')}
+            >
+                <Image 
+                    source={icons.rightArrow}
+                    style={{
+                        width: 15,
+                        height: 15,
+                        tintColor: COLORS.lightGreen1
+                    }}
+                />
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+const RecipeCreatorCardInfo = ({ selectedRecipe }) => {
+    if (Platform.OS === 'ios') {
+        return (
+            <BlurView
+                tint="default"
+                intensity={150}
+                style={styles.recipeCardContainer}
+            >
+                <RecipeCreatorCardDetail
+                    selectedRecipe={selectedRecipe}
+                />
+            </BlurView>
+        )
+    } else {
+        return (
+            <View
+                style={{
+                    ...styles.recipeCardContainer,
+                    backgroundColor: COLORS.transparentGray
+                }}
+            >
+                <RecipeCreatorCardDetail
+                    selectedRecipe={selectedRecipe}
+                />
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    recipeCardContainer: {
+        position: 'absolute',
+        bottom: 10,
+        left: 10,
+        right: 10,
+        height: 80,
+        paddingVertical: SIZES.radius,
+        paddingHorizontal: SIZES.base,
+        borderRadius: SIZES.radius
+    }
+})
 
 const Recipe = ({ navigation, route }) => {
 
@@ -23,7 +131,90 @@ const Recipe = ({ navigation, route }) => {
     React.useEffect(() => {
         let { recipe } = route.params
         setSelectedRecipe(recipe)
-    })
+    }, [])
+
+    function renderHeaderBar() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    height: 60,
+                    flexDirection: 'row',
+                    alignItems: 'flex-end',
+                    justifyContent: 'space-between',
+                    paddingHorizontal: SIZES.padding,
+                    paddingBottom: 10
+                }}
+            >
+                {/* Screen Overlay */}
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: COLORS.transparent,
+                        opacity: scrollY.interpolate({
+                            inputRange: [-HEADER_HEIGHT -100, HEADER_HEIGHT - 70],
+                            outputRange: [0, 1]
+                        }),
+                    }}
+                />
+
+                {/* Header Button Title */}
+
+
+                {/* Back Button */}
+                <TouchableOpacity
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 35,
+                        width: 35,
+                        borderRadius: 18,
+                        borderWidth: 1,
+                        borderColor: COLORS.lightGray,
+                        backgroundColor: COLORS.transparentBlack5 
+                    }}
+                   onPress={() => navigation.goBack()} 
+                >
+                    <Image 
+                        source={icons.back}
+                        style={{
+                            width: 15,
+                            height: 15,
+                            tintColor: COLORS.lightGray
+                        }}
+                    />
+                </TouchableOpacity>
+
+                {/* Bookmark */}
+                <TouchableOpacity
+                    style={{
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 35,
+                        width: 35
+                    }}
+                >
+                    <Image 
+                        source={selectedRecipe?.isBookmark ? icons.bookmarkFilled : icons.bookmark}
+                        style={{
+                            width: 30,
+                            height: 30,
+                            tintColor: COLORS.darkGreen
+                        }}
+                    />
+                </TouchableOpacity>
+                
+
+            </View>
+        )
+    }
 
     function renderRecipeCardHeader() {
         return (
@@ -36,7 +227,7 @@ const Recipe = ({ navigation, route }) => {
                 }}
             >
                 {/* Background Image */}
-                <Animated.Image 
+                <Animated.Image
                     source={selectedRecipe?.image}
                     resizeMode="contain"
                     style={{
@@ -47,7 +238,7 @@ const Recipe = ({ navigation, route }) => {
                                 translateY: scrollY.interpolate({
                                     inputRange: [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
                                     outputRange: [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
-                               })
+                                })
                             },
                             {
                                 scale: scrollY.interpolate({
@@ -59,6 +250,27 @@ const Recipe = ({ navigation, route }) => {
                     }}
                 />
                 {/* Recipe Creator Card */}
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        bottom: 10,
+                        left: 10,
+                        right: 10,
+                        transform: [
+                            {
+                                translateY: scrollY.interpolate({
+                                    inputRange: [0, 170, 250],
+                                    outputRange: [0, 0, 100],
+                                    extrapolate: 'clamp'
+                                })
+                            }
+                        ]
+                    }}
+                >
+                    <RecipeCreatorCardInfo
+                        selectedRecipe={selectedRecipe}
+                    />
+                </Animated.View>
 
             </View>
         )
@@ -82,7 +294,7 @@ const Recipe = ({ navigation, route }) => {
                         {/* Info */}
 
                         {/* ingredient Title */}
-                        
+
                     </View>
                 }
                 scrollEventThrottle={16}
@@ -120,9 +332,9 @@ const Recipe = ({ navigation, route }) => {
                         {/* Description */}
                         <View
                             style={{
-                               flex: 1,
-                               paddingHorizontal: 20,
-                               justifyContent: 'center' 
+                                flex: 1,
+                                paddingHorizontal: 20,
+                                justifyContent: 'center'
                             }}
                         >
                             <Text
@@ -154,6 +366,10 @@ const Recipe = ({ navigation, route }) => {
                     </View>
                 )}
             />
+
+            {/* Header Bar */}
+            {renderHeaderBar()}
+
         </View>
     )
 }
